0c93297665efc70e6d48b85d501aa50fc9eff9ef
halibobo1205 <halibobo1205@gmail.com>
2022-04-20 18:01:04 +0800

    edit test

--- example1.c
+++ example1.c
@@ -7,51 +7,20 @@
 #include "stdio.h"
 
 
-long supply = 1000000000000000000L;
-
-long exchangeToSupply(long balance, long quant) {
+long cal(long balance, long quant) {
     long newBalance = balance + quant;
-    double d1 = 1.0 + (double) (quant) / (double) (newBalance);
-    printf("d1 = %1.64e\n", d1);
-    double d2 = jpow((double )d1, (double )0.0005);
-    printf("d2 = %1.64e\n", d2);
-    double d3 =1.0061363892207217940466534855659119784832000732421875;
-    printf("d3 = %1.64e\n", d3);
-    double d4 = pow(d3, 0.0005);
-    printf("d4 = %1.64e\n", d4);
-    double d5 = pow(1.0061363892207217940466534855659119784832000732421875, 0.0005);
-    printf("d5 = %1.64e\n", d5);
-
-    double issuedSupply = (double) -supply * (1.0 - pow(1.0 + (double) (quant) / (double) (newBalance), 0.0005));
-    long out = (long) issuedSupply;
-    supply += out;
-    return out;
+    double g = pow(1.0 +  (double)29218/4761432,0.0005);
+    printf("g = %1.64e\n", g);
+    double e = pow(1.0 +  (double)29218/newBalance,0.0005);
+    printf("e = %1.64e\n", e);
+    double f = pow(1.0 +  (double)quant/newBalance, 0.0005);
+    printf("f = %1.64e\n", f);
+    return 0;
 }
 
-long exchangeFromSupply(long balance, long supplyQuant) {
-    supply -= supplyQuant;
-    double exchangeBalance = (double) balance * (pow(1.0 + (double) supplyQuant / (double) supply, 2000.0) - 1.0);
-    return (long) exchangeBalance;
-}
-
-long exchange(long sellTokenBalance, long buyTokenBalance, long sellTokenQuant) {
-    long relay = exchangeToSupply(sellTokenBalance, sellTokenQuant);
-    return exchangeFromSupply(buyTokenBalance, relay);
-}
 
 int main() {
-    double y0();
-    printf("y0(1e300) = %1.20e\n", y0(1e300));
-    //double y0();
-    _LIB_VERSION =  _IEEE_;
-    printf("IEEE: y0(1e300) = %1.20e\n",y0(1e300));
-    _LIB_VERSION = _XOPEN_;
-    printf("XOPEN y0(1e300) = %1.20e\n",y0(1e300));
-    _LIB_VERSION = _POSIX_;
-    printf("POSIX y0(1e300) = %1.20e\n",y0(1e300));
-    _LIB_VERSION = _SVID_;
-    printf("SVID  y0(1e300) = %1.20e\n",y0(1e300));
-    printf("%ld\n", exchange(4732214, 2202692725330, 29218) + 608405);
+    cal(4732214,29218);
     exit(0);
 }
 
